18a5a32ba2e3a9de73ef8edfcffe6f7138fbf06f
Hchoudry <102235101+HashimChoudry@users.noreply.github.com>
2026-06-28 13:13:15 +0100

    Spring Scaffolding

---
+++ src/main/java/com/metis/liftcompanion/LiftcompanionApplication.java
@@ -0,0 +1,13 @@
+package com.metis.liftcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftcompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftcompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metis/liftcompanion/LiftcompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metis.liftcompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftcompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
